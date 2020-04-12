5336fc26b11c2c9ad5da690f6ab69d1765c76d2c
Vanni Fan <web.cn@msn.com>
2020-04-12 10:18:28 +0000

    update

--- install.php
+++ install.php
@@ -1,7 +1,17 @@
 <?php
+$tips = 'Please enter a 6-9 digit password:';
+
+if(is_null($argv[1])){
+    $key = random_int(10000000,99999999);
+    $key = getInput("$tips default [{$key}]:", $key);
+}else{
+    $key = $argv[1];
+    if(!preg_match('/^\d{6,9}$/', $key)){
+        $key = getInput($tips, $default);
+    }
+}
+
 $var = chr(random_int(97,122));
-$key = random_int(10000000,99999999);
-$key = getInput("Please enter a 6-9 digit password, default [{$key}]:", $key);
 $str = str_replace(['__VAR__','__KEY__'],[$var, $key],file_get_contents('./hp/Password.zep.tmp'));
 
 file_put_contents('./hp/Password.zep',$str);
@@ -9,14 +19,14 @@ file_put_contents('./hp/Password.zep',$str);
 function getInput($tips, $default){
     echo $tips;
     $f = fopen('php://stdin','r');
-    $s = trim(fgets($f));
+    $s = trim(fgets($f)) ?: $default;
     fclose($f);
-    if(preg_match('/^\d{6,9}$/', $s) || empty($s)){
-        echo "Your input is [{$default}], please remember this password!";
-        return $s ?: $default;
+    if(preg_match('/^\d{6,9}$/', $s)){
+        return $s;
     }else{
         return getInput($tips, $default);
     }
 }
-echo "\n";
+
+echo "\nYour input is [$key], please remember this password!\n";
 system('zephir build');
